5cc8503ddf0c7a65fdfdd448c2855206853ec939
dgrundel <daniel@webpresencepartners.com>
2015-06-26 20:40:35 -0400

    better error handling

--- perf.js
+++ perf.js
@@ -6,6 +6,8 @@ var perf = function(testName) {
 	this.tests = [];
 	this.teardowns = [];
 	this.lastRunTime = 0;
+	this.hasErrors = false;
+	this.errors = {};
 	
 	return this;
 };
@@ -13,64 +15,92 @@ var perf = function(testName) {
 perf.prototype.setIterations = function(iterations) {
 	this.iterations = iterations;
 	return this;
-}
+};
 
 perf.prototype.test = function(callback) {
 	this.tests.push(callback);
 	return this;
-}
+};
 
 perf.prototype.setup = function(callback) {
 	this.setups.push(callback);
 	return this;
-}
+};
 
 perf.prototype.teardown = function(callback) {
 	this.teardowns.push(callback);
 	return this;
-}
+};
+
+perf.prototype.addError = function(type, message) {
+	this.hasErrors = true;
+	this.errors[type] = this.errors[type] || [];
+	this.errors[type].push(message);
+};
+
+perf.prototype.clearErrors = function() {
+	this.hasErrors = false;
+	this.errors = {};
+};
 
 perf.prototype.run = function() {
+	var thisPerf = this;
 	var t0;
 	var t1;
 	var namespace = {};
 	var error = false;
-	var runAll = function(arrayOfFunctions) {
+	var runAll = function(type, arrayOfFunctions) {
 		for(var i = 0, l = arrayOfFunctions.length; i < l; i++) {
 			try {
 				arrayOfFunctions[i](namespace);
 			} catch(e) {
-				if(e.message) {
-					perf.log(e.message, 'error');
-				} else if(typeof e === 'string') {
-					perf.log(e, 'error');
-				}
+				onError(type, e);
 				return false;
 			}
 		}
-	}
-	
-	runAll(this.setups);
+	};
+	var onError = function(type, error) {
+		var errorMessage = 'Unknown error.';
+		if(error.message) {
+			errorMessage = error.message;
+		} else if(typeof e === 'string') {
+			errorMessage = e;
+		}
 
-	t0 = performance.now();
-	for(var iterationIndex = 0; iterationIndex < this.iterations; iterationIndex++) {
-		if(runAll(this.tests) === false) {
-			perf.log('Exception thrown, ' + this.formattedTestName + ' ended prematurely.', 'alert');
-			error = true;
-			break;
+		perf.log(errorMessage, 'error');
+		thisPerf.addError(type, errorMessage);
+	};
+
+	this.clearErrors();
+	
+	if(runAll('setup', this.setups) === false) {
+		perf.log('Exception thrown in setup, ' + this.formattedTestName + ' ended prematurely.', 'alert');
+	}
+
+	if(!this.hasErrors) {
+		t0 = performance.now();
+		for(var iterationIndex = 0; iterationIndex < this.iterations; iterationIndex++) {
+			if(runAll('test', this.tests) === false) {
+				perf.log('Exception thrown in test, ' + this.formattedTestName + ' ended prematurely.', 'alert');
+				break;
+			}
+		}
+		t1 = performance.now();
+	}
+
+	if(!this.hasErrors) {
+		if(runAll('teardown', this.teardowns) === false) {
+			perf.log('Exception thrown in teardown, ' + this.formattedTestName + ' ended prematurely.', 'alert');
 		}
 	}
-	t1 = performance.now();
 
-	runAll(this.teardowns);
-
-	if(error !== true) {
+	if(!this.hasErrors) {
 		this.lastRunTime = (t1 - t0);
 		perf.log(this.formattedTestName + ' (x ' + this.iterations + ') <span class="time">' + this.lastRunTime.toFixed(8) + ' ms</span>');
 	}
 
 	return this;
-}
+};
 
 perf.log = function(message, cssClass) {
 	if(perf.config.logNode) {
@@ -83,7 +113,7 @@ perf.log = function(message, cssClass) {
 	}
 
 	return this;
-}
+};
 
 perf.compare = function() {
 	var fastest = null;
@@ -101,8 +131,11 @@ perf.compare = function() {
 		}
 	}
 
-	perf.log(fastest.formattedTestName + ' is fastest.', 'result');
-}
+	if(fastest) {
+		perf.log(fastest.formattedTestName + ' is fastest.', 'result');
+	}
+	return fastest;
+};
 
 perf.config = {
 	defaultIterations: 500000,
